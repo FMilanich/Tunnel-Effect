3dd493df4c28782d47ff057a19cbcf2c33ef5ea0
Francisco Milanich <franciscomilanich@gmail.com>
2026-05-28 00:43:07 -0300

    window added

---
+++ TunnelDemo.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class TunnelDemo{
+
+    public static void main(String[] args){
+
+        // This is for initializing the window
+        JFrame window = new JFrame ("Tunnel Effect");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(800, 600);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
