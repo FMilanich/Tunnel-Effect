c4342a658e20314be9bc43f8cc869e220fbb5e34
Francisco Milanich <franciscomilanich@gmail.com>
2026-05-28 22:36:09 -0300

    panel connected to window

--- TunnelDemo.java
+++ TunnelDemo.java
@@ -4,10 +4,18 @@ public class TunnelDemo{
 
     public static void main(String[] args){
 
+        // It could be bigger but idk
+        int width = 800;
+        int height = 600;
+
         // This is for initializing the window
         JFrame window = new JFrame ("Tunnel Effect");
+
+        RenderPanel panel = new RenderPanel(width, height);
+        window.add(panel);
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(800, 600);
+        window.setSize(width, height);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
